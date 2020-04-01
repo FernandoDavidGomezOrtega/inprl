774e4ac3d22010c6d5c50a180f621da22002dc8d
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2020-04-01 12:23:33 +0200

    css updates

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,44 +1,47 @@
 </div>
-    </section >
-         <!-- ver si funciona bien así -->
+</section>
+<!-- ver si funciona bien así -->
 
-  <!--==========================
+<!--==========================
   Footer
 ============================-->
 
-  <footer id="footer">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="copyright">
-          Designed by: <a href="https://amazingweb.website/fernando/" target="_blank"><strong>Fernando Gómez Web 2019</strong></a>
-          </div>
-          <div class="credits">
-        
-            <a href="mailto:fernando.amazingweb@gmail.com">fernando.amazingweb@gmail.com</a>
-          </div>
+<footer id="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <!-- Copyright -->
+        <p class="copyright">
+          <a href="https://gomez-fernando.github.io/portfolio/" target="_blank" rel="noopener noreferrer">Developed with
+            &#x1F49A; by Fernando Gómez</a>
+        </p>
+        <!-- Copyright -->
+        <div class="credits">
+
+          <a href="mailto:fernando.amazingweb@gmail.com">fernando.amazingweb@gmail.com</a>
         </div>
       </div>
     </div>
-  </footer>
-  <!-- #footer -->
+  </div>
+</footer>
+<!-- #footer -->
 
-  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+<a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
 
-  <!-- Required JavaScript Libraries -->
-  <script src="lib/jquery/jquery.min.js"></script>
-  <script src="lib/bootstrap/js/bootstrap.min.js"></script>
-  <script src="lib/superfish/hoverIntent.js"></script>
-  <script src="lib/superfish/superfish.min.js"></script>
-  <script src="lib/morphext/morphext.min.js"></script>
-  <script src="lib/wow/wow.min.js"></script>
-  <script src="lib/stickyjs/sticky.js"></script>
-  <script src="lib/easing/easing.js"></script>
+<!-- Required JavaScript Libraries -->
+<script src="lib/jquery/jquery.min.js"></script>
+<script src="lib/bootstrap/js/bootstrap.min.js"></script>
+<script src="lib/superfish/hoverIntent.js"></script>
+<script src="lib/superfish/superfish.min.js"></script>
+<script src="lib/morphext/morphext.min.js"></script>
+<script src="lib/wow/wow.min.js"></script>
+<script src="lib/stickyjs/sticky.js"></script>
+<script src="lib/easing/easing.js"></script>
 
-  <!-- Template Specisifc Custom Javascript File -->
-  <script src="js/custom.js"></script>
+<!-- Template Specisifc Custom Javascript File -->
+<script src="js/custom.js"></script>
 
-  <!-- <script src="contactform/contactform.js"></script> -->
+<!-- <script src="contactform/contactform.js"></script> -->
 
 
 </body>
